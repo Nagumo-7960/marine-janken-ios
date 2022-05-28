888bdf5e408118314a826c1a2168bd76ab352087
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-28 23:56:34 +0900

    画像を表示

--- MarineJanken/MarineJanken/ViewController.swift
+++ MarineJanken/MarineJanken/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var homeImage: UIImageView!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let homeImages = UIImage(named: "marine_home")
+        homeImage?.image = homeImages
     }
 
 
