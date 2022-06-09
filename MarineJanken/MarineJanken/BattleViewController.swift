a0dd99620a79fd1ab83eddb4739ba6e10c97d0e4
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-09 16:13:09 +0900

    水着のお姉さんを表示

--- MarineJanken/MarineJanken/BattleViewController.swift
+++ MarineJanken/MarineJanken/BattleViewController.swift
@@ -11,9 +11,14 @@ class BattleViewController: UIViewController {
 
     @IBOutlet weak var backGround: UIImageView!
     
+    @IBOutlet weak var girl: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let background = UIImage(named: "marine_battle_field")
         backGround?.image = background
+        
+        let beachGirl = UIImage(named: "girl_01")
+        girl?.image = beachGirl
     }
 }
