485634ee5e29ddc13fe81d7f95a34c8563369397
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-11 21:52:58 +0900

    じゃんけんカードの画像の大きさを調整

--- MarineJanken/MarineJanken/BattleViewController.swift
+++ MarineJanken/MarineJanken/BattleViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class BattleViewController: UIViewController {
-
+    
     @IBOutlet weak var backGroundView: UIImageView!
     
     @IBOutlet weak var girlView: UIImageView!
     
     @IBOutlet weak var oppBalloonView: UIImageView!
+    @IBOutlet weak var myBallonView: UIImageView!
+    
+    
+    @IBOutlet weak var guCardView: UIButton!
+    @IBOutlet weak var chokiCardView: UIButton!
+    @IBOutlet weak var paCardView: UIButton!
+    
+    let gu = UIImage(named:"janken_gu")
+    let choki = UIImage(named:"janken_choki")
+    let pa = UIImage(named:"janken_pa")
+    
+    var screenWidth:CGFloat = 0
+    var screenHeight:CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,5 +35,19 @@ class BattleViewController: UIViewController {
         
         let beachGirl = UIImage(named: "girl_01")
         girlView?.image = beachGirl
+        
+        guCardView.setImage(gu, for: .normal)
+        guCardView.imageView?.contentMode = .scaleAspectFit
+        guCardView.contentHorizontalAlignment = .fill
+        guCardView.contentVerticalAlignment = .fill
+        
+        screenWidth = view.frame.size.width
+        screenHeight = view.frame.size.height
+        
+        // Buttonが画面の中央で横幅いっぱいのサイズになるように設定
+        guCardView.frame = CGRect(x:20,
+                                  y:544,
+                                  width:120,
+                                  height:120)
     }
 }
