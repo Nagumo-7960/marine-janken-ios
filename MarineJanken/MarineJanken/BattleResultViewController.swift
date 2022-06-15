4b9d1f78fb80e7467516cfcf693a695f62da26d7
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-15 21:48:58 +0900

    じゃんけんの結果を表示する画面の作成

---
+++ MarineJanken/MarineJanken/BattleResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BattleResultViewController.swift
+//  MarineJanken
+//
+//  Created by なぐも on 2022/06/15.
+//
+
+import UIKit
+
+class BattleResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
