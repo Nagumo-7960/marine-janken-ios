219a16778579aa687d5d20de51e9601c3d487f26
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-19 17:34:42 +0900

    対戦結果画面に背景を表示

--- MarineJanken/MarineJanken/BattleResultViewController.swift
+++ MarineJanken/MarineJanken/BattleResultViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class BattleResultViewController: UIViewController {
     
+    @IBOutlet weak var backgroundView: UIImageView!
+    
     var jankenHands = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(jankenHands)
+        let background = UIImage(named: "marine_battle_field")
+        backgroundView?.image = background
     }
     
 
