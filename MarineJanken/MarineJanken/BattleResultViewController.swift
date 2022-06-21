35beb312b4458871246dd802c40a9c160dcce6d7
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-21 22:22:52 +0900

    じゃんけんの手を選んだ後の遷移後の画面を作成

--- MarineJanken/MarineJanken/BattleResultViewController.swift
+++ MarineJanken/MarineJanken/BattleResultViewController.swift
@@ -9,15 +9,35 @@ import UIKit
 
 class BattleResultViewController: UIViewController {
     
-    @IBOutlet weak var backgroundView: UIImageView!
+    @IBOutlet weak var backGroundView: UIImageView!
+    
+    @IBOutlet weak var girlView: UIImageView!
+    
+    @IBOutlet weak var oppBalloonView: UIImageView!
+    @IBOutlet weak var myBallonView: UIImageView!
     
     var jankenHands = ""
+    
+    @IBOutlet weak var oppTextView: UILabel!
+    @IBOutlet weak var myTextView: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(jankenHands)
         let background = UIImage(named: "marine_battle_field")
-        backgroundView?.image = background
+        backGroundView?.image = background
+        
+        let beachGirl = UIImage(named: "girl_01")
+        girlView?.image = beachGirl
+        
+        let oppBalloon = UIImage(named: "opp_balloon")
+        oppBalloonView.image = oppBalloon
+        
+        let myBallon = UIImage(named: "my_balloon")
+        myBallonView.image = myBallon
+        
+        oppTextView.text = "どれにしようかしら・・・"
+        myTextView.text = "どうしよう・・・"
     }
     
 
