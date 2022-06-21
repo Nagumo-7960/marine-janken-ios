05c19b01abed281a7e7d59aa62711a6671d7ff42
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-21 22:47:19 +0900

    じゃんけん対戦画面で選んだ手に応じた画像を結果画面で表示

--- MarineJanken/MarineJanken/BattleResultViewController.swift
+++ MarineJanken/MarineJanken/BattleResultViewController.swift
@@ -16,10 +16,24 @@ class BattleResultViewController: UIViewController {
     @IBOutlet weak var oppBalloonView: UIImageView!
     @IBOutlet weak var myBallonView: UIImageView!
     
+    @IBOutlet weak var oppHandView: UIImageView!
+    @IBOutlet weak var myHandView: UIImageView!
+    
+    let gu = UIImage(named:"janken_gu")
+    let choki = UIImage(named:"janken_choki")
+    let pa = UIImage(named:"janken_pa")
+    
     var jankenHands = ""
+    var oppHands = ["gu", "choki", "pa"]
+    var oppHand:String = ""
+    
+
     
     @IBOutlet weak var oppTextView: UILabel!
     @IBOutlet weak var myTextView: UILabel!
+    
+    let oppText = ""
+    let myText = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,10 +50,27 @@ class BattleResultViewController: UIViewController {
         let myBallon = UIImage(named: "my_balloon")
         myBallonView.image = myBallon
         
+        oppHandView.image = gu
+        switch jankenHands{
+        case "gu":
+            myHandView.image = gu
+        case "choki":
+            myHandView.image = choki
+        case "pa" :
+            myHandView.image = pa
+        default:
+            myHandView.image = gu
+        }
+        
+        
         oppTextView.text = "どれにしようかしら・・・"
         myTextView.text = "どうしよう・・・"
     }
     
+    func battleResultCheck(hand:String) -> Bool{
+        oppHand = oppHands.randomElement()!
+        return true
+    }
 
 
 }
