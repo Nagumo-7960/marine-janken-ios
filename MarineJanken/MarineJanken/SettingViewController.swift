4185eabf75d22b88c8f0bbdf376f910e71e2637d
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-04 21:32:42 +0900

    CollectionViewの必須メソッドを定義

--- MarineJanken/MarineJanken/SettingViewController.swift
+++ MarineJanken/MarineJanken/SettingViewController.swift
@@ -7,7 +7,16 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
